ca27a975428a6eb5f092e60d809a7ce054419436
Abdelrahman Hamdy <xmbedo@gmail.com>
2024-03-23 12:17:37 +0200

    Edit Mail Header With Logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,6 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
+<img src="https://ucarecdn.com/7ac7bd8f-fb2f-4b4c-8541-9d5c5e7d6cd0/-/preview/200x200/" class="logo" alt="Laravel Logo">
 <h3>
 
 أطلس الذهبية للمصاعد
